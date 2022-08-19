a6c9b599609d6eab8981a63a1f956400a4a8ceb1
George Ball <georgeball95@gmail.com>
2022-08-19 13:53:18 +0100

    Update 02_read data.py

--- pages/02_read data.py
+++ pages/02_read data.py
@@ -7,17 +7,18 @@ deta = Deta(st.secrets["deta_key"])
 # Data to be written to Deta Base
 name = st.text_input("Your name")
 
-#FIND USER
-#db_content = db.fetch().items
-
+#read db
 db = deta.Base("default")
 
-#if name:
-#    db.put({"name": name, "age": age})
-
-"---"
-"Here's everything stored in the database:"
-# This reads all items from the database and displays them to your app.
-# db_content is a list of dictionaries. You can do everything you want with it.
+#get db
 db_content = db.fetch().items
-st.write(db_content)
+
+for i in db_content:
+  
+  if i["username"] == name:
+    
+      st.markdown("your name is {} and you have an account".format(name)
+  else:
+      st.markdown("you do not yet have an account")      
+            
+#st.write(db_content)
