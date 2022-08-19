239082abc72055adc604a7dab42f9d2b0ccddab4
George Ball <georgeball95@gmail.com>
2022-08-19 14:09:46 +0100

    Update 02_read data.py

--- pages/02_read data.py
+++ pages/02_read data.py
@@ -9,18 +9,20 @@ name = st.text_input("Your name")
 
 #read db
 db = deta.Base("default")
-
 #get db
 db_content = db.fetch().items
 
-for i in db_content:
-  
-    #st.markdown(i)
-  
-    if i["name"] == name:
+if name:
+
+    for i in db_content:
+
+        #st.markdown(i)
+
+        if i["name"] == name:
+
+            st.markdown("you have an account")
+        else:
+            st.markdown("you do not yet have an account")      
+
     
-        st.markdown("your name is {} and you have an account".format(name))
-    else:
-        st.markdown("you do not yet have an account")      
-            
-st.write(db_content)
+    #st.write(db_content)
